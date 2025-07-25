5b17c291882108ae52c098ce7ee4bfe542ab7917
GM <mostofart@gmail.com>
2025-07-25 17:34:33 +0600

    Solved 896 - Monotonic Array.

---
+++ 896. Monotonic Array/MonotonicArray.cs
@@ -0,0 +1,21 @@
+public class Solution 
+{
+    public bool IsMonotonic(int[] nums) 
+    {
+        bool increasing = true;
+        bool decreasing = true;
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] > nums[i + 1])
+            {
+                increasing = false;
+            }
+            if (nums[i] < nums[i + 1])
+            {
+                decreasing = false;
+            }
+        }
+        return increasing || decreasing;
+    }
+}
