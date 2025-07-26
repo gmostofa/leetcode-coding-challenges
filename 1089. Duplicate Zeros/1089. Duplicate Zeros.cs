4c390fc8c02b9024a6eeaebc96b1de547f4ef74e
GM <mostofart@gmail.com>
2025-07-26 17:29:25 +0600

    Solved 1089

---
+++ 1089. Duplicate Zeros/1089. Duplicate Zeros.cs
@@ -0,0 +1,25 @@
+namespace DefaultNamespace;
+
+public class Solution 
+{
+    public void DuplicateZeros(int[] arr) 
+    {
+        int n = arr.Length;
+
+        for (int i = 0; i < n - 1; i++) 
+        {
+            if (arr[i] == 0) 
+            {
+                for (int j = n - 1; j > i; j--) 
+                {
+                    arr[j] = arr[j - 1];
+                }
+                if (i + 1 < n) 
+                {
+                    arr[i + 1] = 0;
+                }
+                i++; 
+            }
+        }
+    }
+}
