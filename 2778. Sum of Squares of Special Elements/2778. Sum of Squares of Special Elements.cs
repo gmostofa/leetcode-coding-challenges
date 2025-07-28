8eb12ebd756cefa77525e094e293e4414c9bc8aa
Golam Mostofa <mostofart@gmail.com>
2025-07-28 22:42:21 +0600

    Solved 2778 leetcode problems

---
+++ 2778. Sum of Squares of Special Elements/2778. Sum of Squares of Special Elements.cs
@@ -0,0 +1,20 @@
+﻿namespace DefaultNamespace;
+
+public class Solution 
+{
+    public int SumOfSquares(int[] nums) 
+    {
+        int n = nums.Length;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) 
+        {
+            if (n % (i + 1) == 0) 
+            {
+                sum += nums[i] * nums[i];  // square and add
+            }
+        }
+
+        return sum;
+    }
+}
