8a2f77861658f3e60c4ca167be4bdd900ec9be1f
Golam Mostofa <mostofart@gmail.com>
2025-07-29 21:50:19 +0600

    1662. Check If Two String Arrays are Equivalent

---
+++ 1662. Check If Two String Arrays are Equivalent/1662. Check If Two String Arrays are Equivalent.cs
@@ -0,0 +1,11 @@
+﻿namespace DefaultNamespace;
+
+public class Solution 
+{
+    public bool ArrayStringsAreEqual(string[] word1, string[] word2) 
+    {
+        string s1 = string.Join("", word1);
+        string s2 = string.Join("", word2);
+        return s1 == s2;
+    }
+}
