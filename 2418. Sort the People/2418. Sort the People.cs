599f94772c8490a09d58de0af1f5c925a583b6b9
Golam Mostofa <mostofart@gmail.com>
2025-07-29 23:12:56 +0600

    2418. Sort the People

---
+++ 2418. Sort the People/2418. Sort the People.cs
@@ -0,0 +1,15 @@
+﻿namespace DefaultNamespace;
+
+public class Solution 
+{
+    public string[] SortPeople(string[] names, int[] heights) 
+    {
+        var people = names
+            .Select((name, index) => new { Name = name, Height = heights[index] })
+            .OrderByDescending(p => p.Height) 
+            .Select(p => p.Name)
+            .ToArray();
+        
+        return people;
+    }
+}
