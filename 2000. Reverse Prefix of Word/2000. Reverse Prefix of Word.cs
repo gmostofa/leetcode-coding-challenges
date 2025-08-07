07e23e422f3bddd243e609c2657bc39be5030aee
GM <mostofart@gmail.com>
2025-08-07 13:08:48 +0600

    2000. Reverse Prefix of Word

---
+++ 2000. Reverse Prefix of Word/2000. Reverse Prefix of Word.cs
@@ -0,0 +1,33 @@
+namespace DefaultNamespace;
+
+public class Solution 
+{
+    public string ReversePrefix(string word, char ch) 
+    {
+        Stack<char> stack = new Stack<char>();
+        int index = -1;
+        for (int i = 0; i < word.Length; i++) 
+        {
+            stack.Push(word[i]);
+            if (word[i] == ch) 
+            {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) 
+        {
+            return word;
+        }
+        StringBuilder result = new StringBuilder();
+        while (stack.Count > 0) 
+        {
+            result.Append(stack.Pop());
+        }
+        for (int i = index+1; i < word.Length; i++) 
+        {
+            result.Append(word[i]);
+        }
+        return result.ToString();
+    }
+}
