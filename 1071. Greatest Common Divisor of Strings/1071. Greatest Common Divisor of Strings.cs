bbee493b1e34ea9c818f7b16c58d37019726bfe0
GM <mostofart@gmail.com>
2025-08-08 04:10:49 +0600

    1071. Greatest Common Divisor of Strings

---
+++ 1071. Greatest Common Divisor of Strings/1071. Greatest Common Divisor of Strings.cs
@@ -0,0 +1,21 @@
+namespace DefaultNamespace;
+
+public class Solution 
+{
+    public string GcdOfStrings(string str1, string str2) 
+    {
+        if (str1 + str2 != str2 + str1)
+            return "";
+
+        int gcdLength = GCD(str1.Length, str2.Length);
+        return str1.Substring(0, gcdLength);
+    }
+
+    private int GCD(int a, int b) 
+    {
+        if (b == 0)
+            return a;
+            
+        return GCD(b, a % b);
+    }
+}
