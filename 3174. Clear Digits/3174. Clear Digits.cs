bb214914bebdef38ddb699e135fff632692c15a1
GM <mostofart@gmail.com>
2025-08-07 15:41:06 +0600

    3174. Clear Digits

---
+++ 3174. Clear Digits/3174. Clear Digits.cs
@@ -0,0 +1,24 @@
+namespace DefaultNamespace;
+
+public class Solution 
+{
+    public string ClearDigits(string s) 
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in s) 
+        {
+            if (char.IsLetter(c)) 
+            {
+                stack.Push(c);
+            } 
+            else if (char.IsDigit(c) && stack.Count > 0) 
+            {
+                stack.Pop(); 
+            }
+        }
+
+        
+        return new string(stack.Reverse().ToArray());
+    }
+}
