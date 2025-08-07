c96402043aa1ea196f4477b2cb3cb6c9afacc126
GM <mostofart@gmail.com>
2025-08-07 23:17:32 +0600

    2485. Find the Pivot Integer

---
+++ 2485. Find the Pivot Integer/2485. Find the Pivot Integer.cs
@@ -0,0 +1,14 @@
+namespace DefaultNamespace;
+
+public class Solution 
+{
+    public int PivotInteger(int n)
+    {
+        int totalSum = (n * (n + 1)) / 2;
+        int x = (int)Math.Sqrt(totalSum);
+
+        if (x * x == totalSum)
+            return x;
+        return -1;
+    }
+}
