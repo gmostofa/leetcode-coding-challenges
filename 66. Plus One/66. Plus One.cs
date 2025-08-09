70a3a6fa0b4ad098f23241605483fa2f1466f057
GM <mostofart@gmail.com>
2025-08-09 17:10:56 +0600

    66. Plus One

---
+++ 66. Plus One/66. Plus One.cs
@@ -0,0 +1,20 @@
+namespace DefaultNamespace;
+
+public class Solution 
+{
+    public int[] PlusOne(int[] digits) 
+    {
+        for (int i = digits.Length - 1; i >= 0; i--) 
+        {
+            digits[i]++;              
+            if (digits[i] < 10) 
+            {     
+                return digits;
+            }
+            digits[i] = 0;          
+        }
+        int[] result = new int[digits.Length + 1];
+        result[0] = 1;  
+        return result;
+    }
+}
