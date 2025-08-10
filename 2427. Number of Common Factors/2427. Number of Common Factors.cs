cef6f8581b090c60f79a842f3c1a1f6a5d544ca8
GM <mostofart@gmail.com>
2025-08-10 13:27:22 +0600

    2427. Number of Common Factors

---
+++ 2427. Number of Common Factors/2427. Number of Common Factors.cs
@@ -0,0 +1,19 @@
+namespace DefaultNamespace;
+
+public class Solution 
+{
+    public int CommonFactors(int a, int b) 
+    {
+        int count = 0;
+        int min = Math.Min(a, b); 
+        
+        for (int i = 1; i <= min; i++) 
+        {
+            if (a % i == 0 && b % i == 0) 
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
