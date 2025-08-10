13517076dd5feef4ce07c1fb77e934b382be34fd
Golam Mostofa <mostofart@gmail.com>
2025-08-10 23:32:38 +0600

    2413. Smallest Even Multiple

---
+++ 2413. Smallest Even Multiple/2413. Smallest Even Multiple.cs
@@ -0,0 +1,11 @@
+﻿namespace DefaultNamespace;
+
+public class Solution {
+    public int SmallestEvenMultiple(int n) {
+        if (n % 2 == 0) {
+            return n;
+        } else {
+            return n * 2;
+        }
+    }
+}
