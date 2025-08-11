c2cc25848f7ae7aac35c1827c29cfe50a181d6f1
Golam Mostofa <mostofart@gmail.com>
2025-08-11 09:16:34 +0600

    1365. How Many Numbers Are Smaller Than the Current Number

---
+++ 1365. How Many Numbers Are Smaller Than the Current Number/1365. How Many Numbers Are Smaller Than the Current Number.cs
@@ -0,0 +1,27 @@
+﻿namespace DefaultNamespace;
+
+public class Solution 
+{
+    public int[] SmallerNumbersThanCurrent(int[] nums) 
+    {
+        int[] freq = new int[101]; 
+        
+        foreach (int num in nums) 
+        {
+            freq[num]++;
+        }
+        
+        for (int i = 1; i < 101; i++) 
+        {
+            freq[i] += freq[i - 1];
+        }
+        
+        int[] result = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            result[i] = nums[i] == 0 ? 0 : freq[nums[i] - 1];
+        }
+        
+        return result;
+    }
+}
