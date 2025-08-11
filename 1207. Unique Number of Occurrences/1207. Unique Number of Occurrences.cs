f05d044359309d17965ecf3fc705a28d6c5afefb
Golam Mostofa <mostofart@gmail.com>
2025-08-11 23:15:53 +0600

    1207. Unique Number of Occurrences

---
+++ 1207. Unique Number of Occurrences/1207. Unique Number of Occurrences.cs
@@ -0,0 +1,23 @@
+﻿namespace DefaultNamespace;
+
+public class Solution 
+{
+    public bool UniqueOccurrences(int[] arr) 
+    {
+        Dictionary<int, int> freq = new Dictionary<int, int>();
+        foreach (int num in arr) 
+        {
+            if (freq.ContainsKey(num))
+                freq[num]++;
+            else
+                freq[num] = 1;
+        }
+        HashSet<int> uniqueFreq = new HashSet<int>();
+        foreach (int count in freq.Values) 
+        {
+            if (!uniqueFreq.Add(count))
+                return false;
+        }
+        return true;
+    }
+}
