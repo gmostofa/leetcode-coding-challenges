5e943bb8bd04879593f4702758dd5f37e3f45d35
Golam Mostofa <mostofart@gmail.com>
2025-08-11 22:04:41 +0600

    Solved 728 Self Dividing Numbers

---
+++ 728. Self Dividing Numbers/728. Self Dividing Numbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public IList<int> SelfDividingNumbers(int left, int right)
+    {
+        List<int> result = new List<int>();
+
+        for (int num = left; num <= right; num++)
+        {
+            if (IsSelfDividing(num))
+            {
+                result.Add(num);
+            }
+        }
+
+        return result;
+    }
+
+    private bool IsSelfDividing(int num)
+    {
+        int original = num;
+
+        while (num > 0)
+        {
+            int digit = num % 10; 
+            if (digit == 0 || original % digit != 0)
+            {
+                return false;
+            }
+            num /= 10;
+        }
+
+        return true;
+    }
+}
