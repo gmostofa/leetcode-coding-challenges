3f462a619787309af37286e5456e2cc47d5a2aa1
Golam Mostofa <mostofart@gmail.com>
2025-08-12 22:10:02 +0600

    1528. Shuffle String

---
+++ 1528. Shuffle String/1528. Shuffle String.cs
@@ -0,0 +1,16 @@
+﻿namespace DefaultNamespace;
+
+public class Solution 
+{
+    public string RestoreString(string s, int[] indices) 
+    {
+        char[] result = new char[s.Length];
+        
+        for (int i = 0; i < s.Length; i++) 
+        {
+            result[indices[i]] = s[i];
+        }
+        
+        return new string(result);
+    }
+}
