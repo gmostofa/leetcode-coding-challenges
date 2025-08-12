e8512fad77279f010d69807cdf724ada2cbdcf3c
Golam Mostofa <mostofart@gmail.com>
2025-08-12 09:03:11 +0600

    268 Missing Numbers

---
+++ 268. Missing Number/268. Missing Number.cs
@@ -0,0 +1,18 @@
+﻿namespace DefaultNamespace;
+
+public class Solution 
+{
+    public int MissingNumber(int[] nums) 
+    {
+        int n = nums.Length;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        foreach (int num in nums) 
+        {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+}
