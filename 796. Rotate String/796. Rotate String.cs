5c9e96279d11ef5b5ea1f898d9c32453ce014bb4
Golam Mostofa <mostofart@gmail.com>
2025-08-12 21:39:59 +0600

    796. Rotate String

---
+++ 796. Rotate String/796. Rotate String.cs
@@ -0,0 +1,10 @@
+﻿namespace DefaultNamespace;
+
+public class Solution 
+{
+    public bool RotateString(string s, string goal) 
+    {
+        if (s.Length != goal.Length) return false;
+        return (s + s).Contains(goal);
+    }
+}
