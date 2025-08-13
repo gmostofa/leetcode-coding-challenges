51ee72269f0c49bd6ad7122ce978da01cc52f001
Golam Mostofa <mostofart@gmail.com>
2025-08-13 09:16:30 +0600

    Solved problems

---
+++ 0000_neetcode_blind_75/Arrays & Hashing/242. Valid Anagram/242. Valid Anagram.cs
@@ -0,0 +1,24 @@
+﻿namespace DefaultNamespace;
+
+public class Solution 
+{
+    public bool IsAnagram(string s, string t) 
+    {
+        if (s.Length != t.Length) return false;
+
+        int[] letters = new int[26];
+
+        for (int i = 0; i < s.Length; i++) 
+        {
+            letters[s[i] - 'a']++;
+            letters[t[i] - 'a']--;
+        }
+
+        foreach (int count in letters) 
+        {
+            if (count != 0) return false;
+        }
+
+        return true;
+    }
+}
---
+++ 326. Power of Three/326. Power of Three.cs
@@ -0,0 +1,17 @@
+﻿namespace DefaultNamespace;
+
+public class Solution 
+{
+    public bool IsPowerOfThree(int n)
+    {
+        if (n <= 0) return false;
+
+        while (n % 3 == 0)
+        {
+            n /= 3;
+        }
+
+        return n == 1;
+    }
+
+}
