dbcd85f66e9e02d21f3a1cec8f905075051d4ef3
Golam Mostofa <mostofart@gmail.com>
2025-08-13 22:51:06 +0600

    Solved Two Sum

---
+++ 0000_neetcode_blind_75/Arrays & Hashing/1. Two Sum/1. Two Sum.cs
@@ -0,0 +1,25 @@
+﻿namespace DefaultNamespace;
+
+public class Solution 
+{
+    public int[] TwoSum(int[] nums, int target) 
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            int diff = target - nums[i];
+
+            if (map.ContainsKey(diff)) 
+            {
+                return new int[] { map[diff], i };
+            }
+
+            if (!map.ContainsKey(nums[i])) {
+                map.Add(nums[i], i);
+            }
+        }
+
+        return new int[0]; 
+    }
+}
