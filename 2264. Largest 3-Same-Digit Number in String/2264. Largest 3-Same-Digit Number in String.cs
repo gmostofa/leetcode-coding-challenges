6214d1c1e0f874d6d43f7145881fddebc3c8b625
Golam Mostofa <mostofart@gmail.com>
2025-08-14 08:54:00 +0600

    Daily challenges 2264. Largest 3-Same-Digit Number in String

---
+++ 2264. Largest 3-Same-Digit Number in String/2264. Largest 3-Same-Digit Number in String.cs
@@ -0,0 +1,23 @@
+﻿namespace DefaultNamespace;
+
+public class Solution 
+{
+    public string LargestGoodInteger(string num)
+    {
+        string maxTriplet = "";
+
+        for (int i = 0; i <= num.Length - 3; i++)
+        {
+            if (num[i] == num[i + 1] && num[i] == num[i + 2])
+            {
+                string current = num.Substring(i, 3);
+                if (string.Compare(current, maxTriplet) > 0)
+                {
+                    maxTriplet = current;
+                }
+            }
+        }
+
+        return maxTriplet;
+    }
+}
