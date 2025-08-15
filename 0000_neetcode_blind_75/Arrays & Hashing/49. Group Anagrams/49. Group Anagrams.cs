adcf24483f3313260320d29ff86e525c97ecd824
GM <mostofart@gmail.com>
2025-08-15 14:29:23 +0600

    49. Group Anagrams

---
+++ 0000_neetcode_blind_75/Arrays & Hashing/49. Group Anagrams/49. Group Anagrams.cs
@@ -0,0 +1,25 @@
+namespace DefaultNamespace;
+
+public class Solution 
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<string, List<string>> map = new Dictionary<string, List<string>>();
+
+        foreach (var word in strs)
+        {
+            char[] chars = word.ToCharArray();
+            Array.Sort(chars);
+            string key = new string(chars);
+
+            if (!map.ContainsKey(key))
+            {
+                map[key] = new List<string>();
+            }
+            map[key].Add(word);
+        }
+
+        return new List<IList<string>>(map.Values);
+    }
+
+}
