553ac7924ab86e874a6dd4d41f0292a522c72f38
GM <mostofart@gmail.com>
2025-08-16 20:46:36 +0600

    1323. Maximum 69 Number

---
+++ 1323. Maximum 69 Number/1323. Maximum 69 Number.cs
@@ -0,0 +1,19 @@
+namespace DefaultNamespace;
+
+public class Solution 
+{
+    public int Maximum69Number(int num)
+    {
+        char[] chars = num.ToString().ToCharArray();
+        for (int i = 0; i < chars.Length; i++)
+        {
+            if (chars[i] == '6')
+            {
+                chars[i] = '9';
+                break; 
+            }
+        }
+        return int.Parse(new string(chars));
+    }
+
+}
