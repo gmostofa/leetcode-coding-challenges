75697b835aa5a2696623df668cb9515562fc550a
GM <mostofart@gmail.com>
2025-08-17 16:54:10 +0600

    1748. Sum of Unique Elements

---
+++ 1748. Sum of Unique Elements/1748. Sum of Unique Elements.cs
@@ -0,0 +1,27 @@
+namespace DefaultNamespace;
+
+public class Solution 
+{
+    public int SumOfUnique(int[] nums)
+    {
+        Dictionary<int, int> freq = new Dictionary<int, int>();
+
+        foreach (int num in nums)
+        {
+            if (freq.ContainsKey(num))
+                freq[num]++;
+            else
+                freq[num] = 1;
+        }
+
+        int sum = 0;
+        foreach (var kvp in freq)
+        {
+            if (kvp.Value == 1)
+                sum += kvp.Key;
+        }
+
+        return sum;
+    }
+
+}
