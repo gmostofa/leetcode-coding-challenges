1e5ea62da148472974fb1a132c215d3a67e28b30
Golam Mostofa <mostofart@gmail.com>
2025-08-18 23:22:47 +0600

    42. Trapping Rain Water

---
+++ 42. Trapping Rain Water/42. Trapping Rain Water.cs
@@ -0,0 +1,35 @@
+﻿namespace DefaultNamespace;
+
+public class Solution 
+{
+    public int Trap(int[] height) 
+    {
+        if (height == null || height.Length < 3) return 0;
+        
+        int left = 0;
+        int right = height.Length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int water = 0;
+        
+        while (left < right) 
+        {
+            if (height[left] < height[right]) 
+            {
+                // Water at left is limited by leftMax
+                leftMax = Math.Max(leftMax, height[left]);
+                water += leftMax - height[left];
+                left++;
+            } 
+            else 
+            {
+                // Water at right is limited by rightMax
+                rightMax = Math.Max(rightMax, height[right]);
+                water += rightMax - height[right];
+                right--;
+            }
+        }
+        
+        return water;
+    }
+}
