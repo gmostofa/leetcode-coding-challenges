c593645ed85af364b8e63424f3fc8d646944c870
Golam Mostofa <mostofart@gmail.com>
2025-08-19 22:06:36 +0600

    1991. Find the Middle Index in Array

---
+++ 724. Find Pivot Index/724. Find Pivot Index.cs
@@ -0,0 +1,28 @@
+﻿namespace DefaultNamespace;
+
+public class Solution 
+{
+    public int PivotIndex(int[] nums) 
+    {
+        if (nums == null || nums.Length == 0) return -1;
+    
+        int totalSum = 0;
+        foreach (int num in nums) 
+        {
+            totalSum += num;
+        }
+   
+        int leftSum = 0;
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            int rightSum = totalSum - nums[i] - leftSum;
+            if (leftSum == rightSum) 
+            {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        
+        return -1;
+    }
+}
