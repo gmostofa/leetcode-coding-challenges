fe9bda840b63ec77220add2f3c12b9116e90ce13
Golam Mostofa <mostofart@gmail.com>
2025-08-19 22:20:00 +0600

    1013. Partition Array Into Three Parts With Equal Sum

---
+++ 1013. Partition Array Into Three Parts With Equal Sum/1013. Partition Array Into Three Parts With Equal Sum.cs
@@ -0,0 +1,34 @@
+﻿namespace DefaultNamespace;
+
+public class Solution {
+    public bool CanThreePartsEqualSum(int[] arr) 
+    {
+        if (arr == null || arr.Length < 3) return false;
+        long totalSum = 0;
+        foreach (int num in arr) 
+        {
+            totalSum += num;
+        }
+        
+        if (totalSum % 3 != 0) return false;
+        long target = totalSum / 3;
+        
+        long currentSum = 0;
+        int partsFound = 0;
+        
+        for (int i = 0; i < arr.Length; i++) 
+        {
+            currentSum += arr[i];
+            if (currentSum == target) 
+            {
+                partsFound++;
+                currentSum = 0;
+                if (partsFound == 2 && i < arr.Length - 1) 
+                {
+                    return true;
+                }
+            }
+        }
+        return partsFound >= 3 && currentSum == 0;
+    }
+}
