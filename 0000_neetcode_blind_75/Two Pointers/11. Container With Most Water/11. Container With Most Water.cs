98b209eff857fa00a48a0356ac10b0153974ed02
GM <mostofart@gmail.com>
2025-08-21 14:51:16 +0600

    solving Two pointers

---
+++ 0000_neetcode_blind_75/Two Pointers/11. Container With Most Water/11. Container With Most Water.cs
@@ -0,0 +1,29 @@
+public class Solution 
+{
+    public int MaxArea(int[] height) 
+    {
+        int left = 0;
+        int right = height.Length - 1;
+        int maxArea = 0;
+        
+        while (left < right) 
+        {
+            int width = right - left;
+            int minHeight = Math.Min(height[left], height[right]);
+            int currentArea = width * minHeight;
+            
+            maxArea = Math.Max(maxArea, currentArea);
+            
+            if (height[left] < height[right]) 
+            {
+                left++;
+            } 
+            else 
+            {
+                right--;
+            }
+        }
+        
+        return maxArea;
+    }
+}
