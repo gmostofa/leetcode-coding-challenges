e3a4bedb2d88cce827fdce525152c61036372e30
Golam Mostofa <mostofart@gmail.com>
2025-08-24 23:52:57 +0600

    Solving Needcode Problems

---
+++ 0000_neetcode_blind_75/Sliding Window/121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.cs
@@ -0,0 +1,18 @@
+﻿public class Solution 
+{
+    public int MaxProfit(int[] prices)
+    {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        foreach (int price in prices)
+        {
+            if (price < minPrice)
+                minPrice = price;
+            else
+                maxProfit = Math.Max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+}
---
+++ 0000_neetcode_blind_75/Stack/20. Valid Parentheses/20. Valid Parentheses.cs
@@ -0,0 +1,35 @@
+﻿public class Solution 
+{
+    public bool IsValid(string s) 
+    {
+        
+        Stack<char> leftSymbols = new Stack<char>();
+       
+        foreach (char c in s) 
+        {
+          
+            if (c == '(' || c == '{' || c == '[') 
+            {
+                leftSymbols.Push(c);
+            }
+            else if (c == ')' && leftSymbols.Count>0 && leftSymbols.Peek() == '(') 
+            {
+                leftSymbols.Pop();
+            }
+            else if (c == '}' && leftSymbols.Count>0 && leftSymbols.Peek() == '{') 
+            {
+                leftSymbols.Pop();
+            } 
+            else if (c == ']' && leftSymbols.Count>0 && leftSymbols.Peek() == '[') 
+            {
+                leftSymbols.Pop();
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        if (leftSymbols.Count>0) return false;
+        else return true;
+    }
+}
