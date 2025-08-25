ed7335907379723e039004b93d57385ddf7354b1
Golam Mostofa <mostofart@gmail.com>
2025-08-26 00:12:04 +0600

    69. Sqrt(x)

---
+++ 69. Sqrt(x)/69. Sqrt(x).cs
@@ -0,0 +1,17 @@
+﻿public class Solution 
+{
+    public int MySqrt(int x)
+    {
+        if (x == 0) return 0;
+
+        double r = x;
+        while (true)
+        {
+            double next = (r + x / r) / 2;
+            if (Math.Abs(r - next) < 1e-7) // convergence check
+                break;
+            r = next;
+        }
+        return (int)r;
+    }
+}
