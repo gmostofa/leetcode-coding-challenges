15d70da683390fefa3f1e7c3d3cca864927db8b2
Golam Mostofa <mostofart@gmail.com>
2025-08-26 21:49:30 +0600

    1185. Day of the Week

---
+++ 1185. Day of the Week/1185. Day of the Week.cs
@@ -0,0 +1,8 @@
+﻿public class Solution
+{
+    public string DayOfTheWeek(int day, int month, int year)
+    {
+        DateTime date = new DateTime(year, month, day);
+        return date.DayOfWeek.ToString();
+    }
+}
