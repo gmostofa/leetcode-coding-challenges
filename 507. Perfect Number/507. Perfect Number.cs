cd5427edc0e817168b8787af6e91df96c6626be5
Golam Mostofa <mostofart@gmail.com>
2025-08-26 21:24:09 +0600

    507. Perfect Number

---
+++ 507. Perfect Number/507. Perfect Number.cs
@@ -0,0 +1,9 @@
+﻿public class Solution
+{
+    private static readonly HashSet<int> Perfects = new()
+    {
+        6, 28, 496, 8128, 33550336
+    };
+
+    public bool CheckPerfectNumber(int num) => Perfects.Contains(num);
+}
