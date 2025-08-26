3715f692aa13ff6e9af0f39c7272d0d87b12d476
Golam Mostofa <mostofart@gmail.com>
2025-08-26 21:29:02 +0600

    507. Perfect Number v2

---
+++ 507. Perfect Number/507. Perfect Number_V2.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public bool CheckPerfectNumber(int num)
+    {
+        if (num <= 1) return false;
+
+        int sum = 1; 
+        int limit = (int)Math.Sqrt(num);
+
+        for (int d = 2; d <= limit; d++)
+        {
+            if (num % d == 0)
+            {
+                int q = num / d;
+                sum += d;
+                if (q != d) sum += q;
+            }
+        }
+
+        return sum == num;
+    }
+}
