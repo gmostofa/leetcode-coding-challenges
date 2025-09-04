f0285dc560da4949e3acb842456adccc5f66f0b4
Golam Mostofa <mostofart@gmail.com>
2025-09-04 09:13:54 +0600

    1763. Longest Nice Substring

---
+++ 1763. Longest Nice Substring/1763. Longest Nice Substring.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public string LongestNiceSubstring(string s) {
+        if (s.Length < 2) return "";
+
+        HashSet<char> set = new HashSet<char>(s);
+
+        for (int i = 0; i < s.Length; i++) {
+            char c = s[i];
+            if (!(set.Contains(char.ToLower(c)) && set.Contains(char.ToUpper(c)))) {
+                string left = LongestNiceSubstring(s.Substring(0, i));
+                string right = LongestNiceSubstring(s.Substring(i + 1));
+                return left.Length >= right.Length ? left : right;
+            }
+        }
+
+        return s;
+    }
+}
