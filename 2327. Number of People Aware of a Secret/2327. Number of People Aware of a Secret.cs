21d6e601c446341a504c4ebf0a4f65200b8532de
GM <mostofart@gmail.com>
2025-09-09 17:41:32 +0600

    2327

---
+++ 2327. Number of People Aware of a Secret/2327. Number of People Aware of a Secret.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int PeopleAwareOfSecret(int n, int delay, int forget) {
+        const int MOD = 1000000007;
+        int[] dp = new int[n + 1];
+        dp[1] = 1; // On day 1, one person knows the secret.
+        
+        long totalPeopleKnowing = 1; // Total number of people who currently know the secret.
+        long peopleSharing = 0; // Number of people available to share the secret.
+        
+        for (int day = 2; day <= n; day++) {
+            // People starting to share on this day.
+            if (day - delay >= 1) {
+                peopleSharing = (peopleSharing + dp[day - delay]) % MOD;
+            }
+            // People forgetting the secret on this day.
+            if (day - forget >= 1) {
+                peopleSharing = (peopleSharing - dp[day - forget] + MOD) % MOD;
+                totalPeopleKnowing = (totalPeopleKnowing - dp[day - forget] + MOD) % MOD;
+            }
+            
+            // Update the number of people who know the secret today.
+            dp[day] = (int)peopleSharing;
+            totalPeopleKnowing = (totalPeopleKnowing + dp[day]) % MOD;
+        }
+        
+        return (int)totalPeopleKnowing;
+    }
+}
