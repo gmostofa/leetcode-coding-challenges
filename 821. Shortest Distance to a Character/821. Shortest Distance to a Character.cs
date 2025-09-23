7b9c585866afc1b8997659861911c7e62e1cd860
GM <mostofart@gmail.com>
2025-09-23 17:51:51 +0600

    Solved 821

---
+++ 821. Shortest Distance to a Character/821. Shortest Distance to a Character.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] ShortestToChar(string s, char c) {
+        int n = s.Length;
+        int[] result = new int[n];
+        int prev = int.MinValue / 2;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == c) prev = i;
+            result[i] = i - prev;
+        }
+
+        prev = int.MaxValue / 2;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == c) prev = i;
+            result[i] = Math.Min(result[i], prev - i);
+        }
+
+        return result;
+    }
+}
