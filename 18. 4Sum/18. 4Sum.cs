90f0328e5e6456e69d237e6570cf65e690a76585
Golam Mostofa <mostofart@gmail.com>
2025-09-26 13:10:17 +0600

    Create 18. 4Sum.cs

---
+++ 18. 4Sum/18. 4Sum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public IList<IList<int>> FourSum(int[] nums, int target) {
+        Array.Sort(nums); // Step 1: Sort the array
+        IList<IList<int>> result = new List<IList<int>>();
+        int n = nums.Length;
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates for i
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // Skip duplicates for j
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        result.Add(new List<int>{ nums[i], nums[j], nums[left], nums[right] });
+
+                        // Skip duplicates for left
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        // Skip duplicates for right
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    }
+                    else if (sum < target) {
+                        left++;
+                    }
+                    else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
