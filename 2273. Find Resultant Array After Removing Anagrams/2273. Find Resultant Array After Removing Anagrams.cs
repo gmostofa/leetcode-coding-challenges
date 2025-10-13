76e884cc2454e974e45a908693d473ae238542d0
Golam Mostofa <mostofart@gmail.com>
2025-10-13 22:35:31 +0600

    Create 2273. Find Resultant Array After Removing Anagrams.cs

---
+++ 2273. Find Resultant Array After Removing Anagrams/2273. Find Resultant Array After Removing Anagrams.cs
@@ -0,0 +1,21 @@
+﻿namespace DefaultNamespace;
+
+public class Solution {
+    public IList<string> RemoveAnagrams(string[] words) {
+        List<string> result = new List<string>();
+        string prev = "";
+
+        foreach (var word in words) {
+            char[] arr = word.ToCharArray();
+            Array.Sort(arr);
+            string sorted = new string(arr);
+
+            if (sorted != prev) {
+                result.Add(word);
+                prev = sorted;
+            }
+        }
+
+        return result;
+    }
+}
