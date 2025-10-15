27f6ecc78df42fc1dff7f1f17d7b7dc2d5b388f9
Golam Mostofa <mostofart@gmail.com>
2025-10-15 21:33:26 +0600

    Create 78. Subsets.cs

---
+++ 78. Subsets/78. Subsets.cs
@@ -0,0 +1,23 @@
+﻿public class Solution {
+    public IList<IList<int>> Subsets(int[] nums) {
+        List<IList<int>> result = new List<IList<int>>();
+        List<int> current = new List<int>();
+
+        void Backtrack(int start) {
+            // Add current subset (copy)
+            result.Add(new List<int>(current));
+
+            for (int i = start; i < nums.Length; i++) {
+                // Choose
+                current.Add(nums[i]);
+                // Explore
+                Backtrack(i + 1);
+                // Un-choose (backtrack)
+                current.RemoveAt(current.Count - 1);
+            }
+        }
+
+        Backtrack(0);
+        return result;
+    }
+}
