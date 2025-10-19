5a33846b89f5550d8eba672b17d50ed7aa96395e
Golam Mostofa <mostofart@gmail.com>
2025-10-19 21:45:15 +0600

    Create 1625. Lexicographically Smallest String After Applying Operations.cs

---
+++ 1625. Lexicographically Smallest String After Applying Operations/1625. Lexicographically Smallest String After Applying Operations.cs
@@ -0,0 +1,38 @@
+﻿public class Solution {
+    public string FindLexSmallestString(string s, int a, int b) {
+        HashSet<string> visited = new HashSet<string>();
+        Queue<string> queue = new Queue<string>();
+        
+        string smallest = s;
+        queue.Enqueue(s);
+        visited.Add(s);
+
+        while (queue.Count > 0) {
+            string curr = queue.Dequeue();
+            if (String.Compare(curr, smallest) < 0)
+                smallest = curr;
+
+            // Operation 1: Add 'a' to digits at odd indices
+            char[] chars = curr.ToCharArray();
+            for (int i = 1; i < chars.Length; i += 2) {
+                int digit = (chars[i] - '0' + a) % 10;
+                chars[i] = (char)(digit + '0');
+            }
+            string added = new string(chars);
+
+            // Operation 2: Rotate right by b
+            string rotated = curr.Substring(curr.Length - b) + curr.Substring(0, curr.Length - b);
+
+            if (!visited.Contains(added)) {
+                visited.Add(added);
+                queue.Enqueue(added);
+            }
+            if (!visited.Contains(rotated)) {
+                visited.Add(rotated);
+                queue.Enqueue(rotated);
+            }
+        }
+
+        return smallest;
+    }
+}
