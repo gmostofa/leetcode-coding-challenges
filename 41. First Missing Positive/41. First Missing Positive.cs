80be5a6c352f937c38ba0598b2defa59514cb8e0
Golam Mostofa <mostofart@gmail.com>
2025-10-24 19:02:19 +0600

    Update 41. First Missing Positive.cs

--- 41. First Missing Positive/41. First Missing Positive.cs
+++ 41. First Missing Positive/41. First Missing Positive.cs
@@ -4,14 +4,12 @@
         
         for (int i = 0; i < n; i++) {
             while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
-                // Swap nums[i] to its correct position
                 int temp = nums[nums[i] - 1];
                 nums[nums[i] - 1] = nums[i];
                 nums[i] = temp;
             }
         }
         
-        // Now check which index is missing
         for (int i = 0; i < n; i++) {
             if (nums[i] != i + 1)
                 return i + 1;
