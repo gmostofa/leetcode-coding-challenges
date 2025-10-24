5196638f93df457aa5a5bf7d12c61fa9ab684fc2
Golam Mostofa <mostofart@gmail.com>
2025-10-24 19:00:46 +0600

    Create 41. First Missing Positive.cs

---
+++ 41. First Missing Positive/41. First Missing Positive.cs
@@ -0,0 +1,22 @@
+﻿public class Solution {
+    public int FirstMissingPositive(int[] nums) {
+        int n = nums.Length;
+        
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                // Swap nums[i] to its correct position
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        
+        // Now check which index is missing
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1)
+                return i + 1;
+        }
+        
+        return n + 1;
+    }
+}
