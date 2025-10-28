e8ec4af7812267f0463c77a7ffbf21ff56baba55
Golam Mostofa <mostofart@gmail.com>
2025-10-28 09:24:19 +0600

    Create 3354. Make Array Elements Equal to Zero.cs

---
+++ 3354. Make Array Elements Equal to Zero/3354. Make Array Elements Equal to Zero.cs
@@ -0,0 +1,24 @@
+﻿public class Solution {
+    public int CountValidSelections(int[] nums) {
+        int n = nums.Length;
+        long total = 0;
+        foreach (int x in nums) total += x;
+
+        long leftSum = 0;
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                long rightSum = total - leftSum;
+                if (leftSum == rightSum) {
+                    result += 2;
+                } else if (Math.Abs(leftSum - rightSum) == 1) {
+                    result += 1;
+                }
+            }
+            leftSum += nums[i];
+        }
+
+        return result;
+    }
+}
