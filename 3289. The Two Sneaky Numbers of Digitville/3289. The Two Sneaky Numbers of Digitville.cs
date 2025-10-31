a872858fd99fbb3297f00e0235fa5712469fafa8
Golam Mostofa <mostofart@gmail.com>
2025-10-31 10:23:50 +0600

    Create 3289. The Two Sneaky Numbers of Digitville.cs

---
+++ 3289. The Two Sneaky Numbers of Digitville/3289. The Two Sneaky Numbers of Digitville.cs
@@ -0,0 +1,19 @@
+﻿public class Solution {
+    public int[] GetSneakyNumbers(int[] nums) {
+        int n = nums.Length;
+        int maxVal = nums.Length;
+        int[] count = new int[maxVal + 1];
+        List<int> result = new List<int>(2);
+
+        foreach (int x in nums) {
+            count[x]++;
+            if (count[x] == 2) {
+                result.Add(x);
+                if (result.Count == 2)
+                    break;
+            }
+        }
+
+        return result.ToArray();
+    }
+}
