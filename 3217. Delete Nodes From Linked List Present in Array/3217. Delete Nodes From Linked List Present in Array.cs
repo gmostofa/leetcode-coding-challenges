147ac7ab290f70c8c835b1071b78ef5425403185
Golam Mostofa <mostofart@gmail.com>
2025-11-01 14:00:48 +0600

    Create 3217. Delete Nodes From Linked List Present in Array.cs

---
+++ 3217. Delete Nodes From Linked List Present in Array/3217. Delete Nodes From Linked List Present in Array.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public ListNode ModifiedList(int[] nums, ListNode head) {
+        var set = new HashSet<int>(nums);
+        var dummy = new ListNode(0, head);
+        var current = dummy;
+
+        while (current.next != null) {
+            if (set.Contains(current.next.val)) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
