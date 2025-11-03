2d06c8429e7bfebaac129ceb5090b6fdddc0e7fc
Golam Mostofa <mostofart@gmail.com>
2025-11-03 10:12:48 +0600

    Create 1578. Minimum Time to Make Rope Colorful.cs

---
+++ 1578. Minimum Time to Make Rope Colorful/1578. Minimum Time to Make Rope Colorful.cs
@@ -0,0 +1,17 @@
+﻿public class Solution {
+    public int MinCost(string colors, int[] neededTime) {
+        int totalTime = 0;
+
+        for (int i = 1; i < colors.Length; i++) {
+            if (colors[i] == colors[i - 1]) {
+                // Remove the smaller one (less time)
+                totalTime += Math.Min(neededTime[i], neededTime[i - 1]);
+                
+                // Keep the larger one for comparison with the next balloon
+                neededTime[i] = Math.Max(neededTime[i], neededTime[i - 1]);
+            }
+        }
+
+        return totalTime;
+    }
+}
