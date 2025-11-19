ac2baaf8c9ee61f22ce4f38090d1202644552df1
Golam Mostofa <mostofart@gmail.com>
2025-11-19 21:39:24 +0600

    Update 2413. Smallest Even Multiple.cs

--- 2413. Smallest Even Multiple/2413. Smallest Even Multiple.cs
+++ 2413. Smallest Even Multiple/2413. Smallest Even Multiple.cs
@@ -2,7 +2,8 @@
 
 public class Solution {
     public int SmallestEvenMultiple(int n) {
-        if (n % 2 == 0) {
+        if (n % 2 == 0) 
+        {
             return n;
         } else {
             return n * 2;
