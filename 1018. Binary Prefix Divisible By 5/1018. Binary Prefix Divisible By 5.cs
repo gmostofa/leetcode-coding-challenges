6b0da50719e1cb580f6a88008e313dfd319af465
Golam Mostofa <mostofart@gmail.com>
2025-11-24 21:53:11 +0600

    Create 1018. Binary Prefix Divisible By 5.cs

---
+++ 1018. Binary Prefix Divisible By 5/1018. Binary Prefix Divisible By 5.cs
@@ -0,0 +1,13 @@
+﻿public class Solution {
+    public IList<bool> PrefixesDivBy5(int[] nums) {
+        List<bool> result = new List<bool>(nums.Length);
+        int value = 0;  // store prefix mod 5 only
+
+        foreach (int bit in nums) {
+            value = (value * 2 + bit) % 5;
+            result.Add(value == 0);
+        }
+
+        return result;
+    }
+}
