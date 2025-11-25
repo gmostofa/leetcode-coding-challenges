8b8b16bfe3c985e4016a27c68a0b39e628e5d899
Golam Mostofa <mostofart@gmail.com>
2025-11-25 22:21:28 +0600

    Create 1015. Smallest Integer Divisible by K.cs

---
+++ 1015. Smallest Integer Divisible by K/1015. Smallest Integer Divisible by K.cs
@@ -0,0 +1,17 @@
+﻿public class Solution {
+    public int SmallestRepunitDivByK(int K) {
+        // if K has factor 2 or 5 → impossible
+        if (K % 2 == 0 || K % 5 == 0)
+            return -1;
+
+        int num = 0;
+
+        for (int length = 1; length <= K; length++) {
+            num = (num * 10 + 1) % K;
+            if (num == 0)
+                return length;
+        }
+
+        return -1; // should never reach here for valid K
+    }
+}
