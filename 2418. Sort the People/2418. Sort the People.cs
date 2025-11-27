5cca81ebeea0700022c8e213e712f231cd853dea
Golam Mostofa <mostofart@gmail.com>
2025-11-27 21:20:39 +0600

    Update 2418. Sort the People.cs

--- 2418. Sort the People/2418. Sort the People.cs
+++ 2418. Sort the People/2418. Sort the People.cs
@@ -4,6 +4,7 @@ public class Solution
 {
     public string[] SortPeople(string[] names, int[] heights) 
     {
+        
         var people = names
             .Select((name, index) => new { Name = name, Height = heights[index] })
             .OrderByDescending(p => p.Height) 
