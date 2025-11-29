8cfe419e6d00d0245f813f688a9eae12de166e38
Golam Mostofa <mostofart@gmail.com>
2025-11-29 23:09:28 +0600

    Create 3512. Minimum Operations to Make Array Sum Divisible by K.cs

---
+++ 3512. Minimum Operations to Make Array Sum Divisible by K/3512. Minimum Operations to Make Array Sum Divisible by K.cs
@@ -0,0 +1,7 @@
+﻿public class Solution {
+    public int MinOperations(int[] nums, int k) {
+        long sum = 0;
+        foreach (int x in nums) sum += x;
+        return (int)(sum % k); // remainder is how many single-unit decrements needed
+    }
+}
