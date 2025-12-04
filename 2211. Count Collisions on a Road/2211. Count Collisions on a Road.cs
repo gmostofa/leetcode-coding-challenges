97f6c5b3ca8f4b2fd173414c2c61737bd4100cf9
Golam Mostofa <mostofart@gmail.com>
2025-12-04 09:34:32 +0600

    Create 2211. Count Collisions on a Road.cs

---
+++ 2211. Count Collisions on a Road/2211. Count Collisions on a Road.cs
@@ -0,0 +1,24 @@
+﻿public class Solution {
+    public int CountCollisions(string directions) {
+        char[] s = directions.ToCharArray();
+        int n = s.Length;
+
+        int left = 0, right = n - 1;
+
+        // Skip cars going left from the beginning (they leave the road)
+        while (left < n && s[left] == 'L')
+            left++;
+
+        // Skip cars going right from the end (they leave the road)
+        while (right >= 0 && s[right] == 'R')
+            right--;
+
+        int collisions = 0;
+
+        for (int i = left; i <= right; i++)
+            if (s[i] != 'S')
+                collisions++;
+
+        return collisions;
+    }
+}
