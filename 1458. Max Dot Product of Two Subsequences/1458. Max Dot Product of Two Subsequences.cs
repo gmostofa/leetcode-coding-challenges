7120700250d8ba421b263b9f5421c2d879ec6323
Golam Mostofa <mostofart@gmail.com>
2026-01-08 09:58:20 +0600

    Create 1458. Max Dot Product of Two Subsequences.cs

---
+++ 1458. Max Dot Product of Two Subsequences/1458. Max Dot Product of Two Subsequences.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public int MaxDotProduct(int[] nums1, int[] nums2)
+    {
+        int n = nums1.Length;
+        int m = nums2.Length;
+
+        int[,] dp = new int[n + 1, m + 1];
+
+        // Initialize with very small value
+        for (int i = 0; i <= n; i++)
+        for (int j = 0; j <= m; j++)
+            dp[i, j] = int.MinValue;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                int product = nums1[i - 1] * nums2[j - 1];
+
+                dp[i, j] = Math.Max(
+                    product,
+                    Math.Max(
+                        dp[i - 1, j - 1] == int.MinValue
+                            ? product
+                            : dp[i - 1, j - 1] + product,
+                        Math.Max(dp[i - 1, j], dp[i, j - 1])
+                    )
+                );
+            }
+        }
+
+        return dp[n, m];
+    }
+}
