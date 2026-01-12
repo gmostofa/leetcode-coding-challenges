c61d1eabc82e15b965085e94a5a7185179a66233
Golam Mostofa <mostofart@gmail.com>
2026-01-12 22:10:44 +0600

    Create 1266. Minimum Time Visiting All Points.cs

---
+++ 1266. Minimum Time Visiting All Points/1266. Minimum Time Visiting All Points.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public int MinTimeToVisitAllPoints(int[][] points)
+    {
+        int totalTime = 0;
+
+        for (int i = 1; i < points.Length; i++)
+        {
+            int dx = Math.Abs(points[i][0] - points[i - 1][0]);
+            int dy = Math.Abs(points[i][1] - points[i - 1][1]);
+
+            totalTime += Math.Max(dx, dy);
+        }
+
+        return totalTime;
+    }
+}
