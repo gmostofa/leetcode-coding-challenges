44867a6908e3397405ace3004dcabe847bf8a1d7
Golam Mostofa <mostofart@gmail.com>
2026-02-22 23:11:14 +0600

    Create 868. Binary Gap.cs

---
+++ 868. Binary Gap/868. Binary Gap.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int BinaryGap(int n)
+    {
+        int maxGap = 0;
+        int lastPosition = -1;
+        int position = 0;
+
+        while (n > 0)
+        {
+            if ((n & 1) == 1)
+            {
+                if (lastPosition != -1)
+                {
+                    maxGap = Math.Max(maxGap, position - lastPosition);
+                }
+                lastPosition = position;
+            }
+
+            n >>= 1;
+            position++;
+        }
+
+        return maxGap;
+    }
+}
